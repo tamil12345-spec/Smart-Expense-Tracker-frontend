9ec09e8d72b6e945ad81e71ba1fee9ffe2279568
tamilarasi <tamilarasi@gmail.com>
2026-09-19 22:54:58 +0530

    initial

--- src/Services/api.js
+++ src/Services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://smart-expense-tracker-backend-8uns.onrender.com",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 api.interceptors.request.use(
